0eff3d0314fd1974d4ddb4101999e94aebf308c4
Ankush Ujawane <multiverse1808@gmail.com>
2026-01-29 21:27:39 +0530

    changes in hooks

--- frontend/src/hooks/useSocket.js
+++ frontend/src/hooks/useSocket.js
@@ -2,11 +2,7 @@ import { io } from "socket.io-client";
 
 const BACKEND_URL = import.meta.env.VITE_API_URL;
 
-if (!BACKEND_URL) {
-  throw new Error("VITE_API_URL is not defined");
-}
-
 export const socket = io(BACKEND_URL, {
-  transports: ["websocket"],
+  transports: ["websocket", "polling"],
   autoConnect: true,
 });
